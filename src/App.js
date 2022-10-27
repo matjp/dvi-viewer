7ec7f6ad60eff198bcfdbaacf540f4bf52cb377b
Matthew J. Penwill <[email protected]>
2022-10-27 14:45:41 +1300

    Update font paths

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import { useEffect, useRef, useState } from 'react';
 import { dviDecode } from '@matjp/dvi-decode';
 import opentype from 'opentype.js';
 
-const luaFontPath = 'lua-font-files';
+const luaFontPath = '/dvi-viewer/lua-font-files';
 
 function SelectFileButton(props) {
   const handleChange = (e) => {
